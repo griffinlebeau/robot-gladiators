23db1eab03343c339130f9153f4e099229457fa2
Griffin <griffinlebeau2@gmail.com>
2022-05-09 18:13:35 -0500

    more changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,36 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10 + 10);
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked" + enemyName + ". " + enemyName + " now has " + " health remaining."
+    );
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked" + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " has died!");
+    }
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+};
 
-console.log("Our robot's name is " + playerName);
-
-function fight() {
-    window.alert("The fight has begun!");
-}
-
-// fight();
+fight()
+;
